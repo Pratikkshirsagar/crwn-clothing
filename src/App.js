e0ff65a85c59654844afeb166b7952cadc99c5d3
Pratik Kshirsagar <pratikkshirsagar@foliosolutions.in>
2020-03-31 16:50:53 +0530

    shopping card component added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-
-function Hats(props) {
-  return (
-    <div>
-      <button onClick={() => props.history.push('/topic')}>Take me</button>
-      <h1>hates</h1>
-    </div>
-  );
-}
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route exact path="/hats" component={Hats}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
       </Switch>
     </div>
   );
