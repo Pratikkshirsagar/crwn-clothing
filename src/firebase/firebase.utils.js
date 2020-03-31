8bb881d45bfa57d73552365ff4cedf97d693c60e
Pratik Kshirsagar <pratikkshirsagar@foliosolutions.in>
2020-03-31 22:36:02 +0530

    files added

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,15 +3,15 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: 'AIzaSyA_JzCR2dBv8khOchHd_Mg99OJ93WBILY4',
+  authDomain: 'test-ocr-259809.firebaseapp.com',
+  databaseURL: 'https://test-ocr-259809.firebaseio.com',
+  projectId: 'test-ocr-259809',
+  storageBucket: 'test-ocr-259809.appspot.com',
+  messagingSenderId: '276072172556',
+  appId: '1:276072172556:web:1fe08277fc7a2891c57229',
+  measurementId: 'G-KJP10X8HLD'
 };
-
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
