6a6abe87c412a29b8d2a8e11aac8f4d0a47b7748
Pratik Kshirsagar <pratikkshirsagar@foliosolutions.in>
2020-03-31 15:42:09 +0530

    withRouter added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+function Hats(props) {
+  return (
+    <div>
+      <button onClick={() => props.history.push('/topic')}>Take me</button>
+      <h1>hates</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/hats" component={Hats}></Route>
+      </Switch>
     </div>
   );
 }
