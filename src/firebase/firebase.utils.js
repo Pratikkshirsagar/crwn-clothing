23b1b290e6a692b44e9614c413b1cd1e2cfc9ac5
Pratik Kshirsagar <pratikkshirsagar@foliosolutions.in>
2020-04-01 13:07:17 +0530

    user added to database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,34 @@ const config = {
   appId: '1:276072172556:web:1fe08277fc7a2891c57229',
   measurementId: 'G-KJP10X8HLD'
 };
+
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
