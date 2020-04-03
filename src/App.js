897f67f4974be4b91336ceed66c953fd6d9ac0aa
Pratikkshirsagar <meetpratiik@gmail.com>
2020-04-03 15:27:02 +0530

    maping of state done

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <BrowserRouter>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
